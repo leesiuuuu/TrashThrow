16fabe4281fb01927d9b3b0b517c84f03ff5260e
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-09-02 20:37:31 +0900

    사운드 이펙트 추가 및 새로운 맵 위치 선정

--- TrashThrow_Re/Assets/Scripts/BinCollider.cs
+++ TrashThrow_Re/Assets/Scripts/BinCollider.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class BinCollider : MonoBehaviour
 {
+    public int BasicScore = 100;
     [SerializeField]
     private ScoreUI scoreUI;
     [SerializeField]
     private ParticleSystem particle;
     private void OnTriggerEnter(Collider other)
     {
-        TrashInfo info = other.GetComponent<TrashInfo>();
-        TrashSO trashSO = info.trashSO;
-        particle.Play();
-        scoreUI.gameObject.SetActive(true);
-        scoreUI.ShowScore(100 * (3 - (int)trashSO.Size), 50 * Mathf.Abs(5 - info.CollideCount));
-        info.Respawn();
+        if (other.gameObject.CompareTag("Obj"))
+        {
+            TrashInfo info = other.GetComponent<TrashInfo>();
+            TrashSO trashSO = info.trashSO;
+            particle.Play();
+            scoreUI.gameObject.SetActive(true);
+            scoreUI.ShowScore(BasicScore * (3 - (int)trashSO.Size), 50 * Mathf.Abs(5 - info.CollideCount));
+            info.Respawn();
+        }
     }
 }
